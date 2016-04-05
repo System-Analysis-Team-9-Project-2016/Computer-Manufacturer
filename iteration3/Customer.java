5787b165dadf93bbaae7f895f5938a38c8114434
DarrenStack <13129783@studentmail.ul.ie>
2016-04-05 12:14:48 +0100

    Added files via upload

--- iteration3/Customer.java
+++ iteration3/Customer.java
@@ -1,54 +1,21 @@
+
 import java.util.ArrayList;
 
-public class Customer {
+public class Customer extends User {
 	
-	private String firstName;
-	private String surname;
-	private String email;
-	private String password;
+	private int customerId;
 	private String creditCardNumber;
-	private ArrayList<String> orders;
+	private ArrayList<Integer> customerOrders;
 	
-	Customer(String firstName , String surname, String email, String password, String creditCardNumber){
-		this.firstName = firstName;
-		this.surname = surname;
-		this.email = email;
-		this.password = password;
+	public Customer(int id , String name , String email, String password, String creditCardNumber){
+		super(name ,email , password);
+		this.customerId = id;
 		this.creditCardNumber = creditCardNumber;
 		
-	}
-
-	public String getFirstName() {
-		return firstName;
-	}
-
-	public void setFirstName(String name) {
-		this.firstName = name;
+		
 	}
 	
-	public void setSurname(String name) {
-		this.surname = name;
-	}
-	
-	public String getSurname() {
-		return surname;
-	}
 
-	public String getEmail() {
-		return email;
-	}
-
-	public void setEmail(String email) {
-		this.email = email;
-	}
-
-	public String getPassword() {
-		return password;
-	}
-
-	public void setPassword(String password) {
-		this.password = password;
-	}
 
 	public String getCreditCardNumber() {
 		return creditCardNumber;
@@ -58,14 +25,32 @@ public class Customer {
 		this.creditCardNumber = creditCardNumber;
 	}
 	
-	public ArrayList<String> getOrders(){
-		return orders;
+	public String getCustomersOrders(){
+		String customerOrderIdList = "";
+		for (int i = 0; i < customerOrders.size(); i++) {
+			customerOrderIdList += customerOrders.get(i);
+		}
+		if (customerOrderIdList == "") return "No orders for this customer";
+		return customerOrderIdList;
 	}
 	
-	//Replace String with order
-	public void addToOrders(String o){
-		orders.add(o);
+	public void addToCustomersOrders(int orderId) {
+		customerOrders.add(orderId);
 	}
+
+
+
+	public int getCustomerId() {
+		return customerId;
+	}
+
+
+
+	public void setCustomerId(int customerId) {
+		this.customerId = customerId;
+	}
+	
+	
 	
 	
 	
---
+++ iteration3/User.java
@@ -0,0 +1,42 @@
+
+
+public class User {
+	
+	private String username;
+	private String email;
+	private String password;
+	
+	public User(String username , String email, String password){
+		this.username = username;
+		this.email = email;
+		this.password = password;
+		
+	}
+
+	public String getUsername() {
+		return username;
+	}
+
+	public void setuserName(String name) {
+		this.username = name;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	
+	
+
+}
