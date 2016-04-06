6f08c518b47caccd08a0d012839e787964e4bab3
DarrenStack <13129783@studentmail.ul.ie>
2016-04-06 21:06:05 +0100

    Added files via upload

---
+++ iteration3/Observer.java
@@ -0,0 +1,6 @@
+package systemAnalysis;
+
+public interface Observer {
+
+	public void Update(Product p);
+}
---
+++ iteration3/Subject.java
@@ -0,0 +1,7 @@
+package systemAnalysis;
+
+public interface Subject {
+	public void registerObserver(Observer O);
+	public void notifyObservers();
+
+}
