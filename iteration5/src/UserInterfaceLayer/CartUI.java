0223491d1cd80fe33f0a6de30c9265d36710be32
Cian Prendergast <cianp94@gmail.com>
2016-04-13 16:57:15 +0100

    Update CartUI.java

--- iteration5/src/UserInterfaceLayer/CartUI.java
+++ iteration5/src/UserInterfaceLayer/CartUI.java
@@ -1,11 +1,15 @@
 package UserInterfaceLayer;
 
+import java.awt.BorderLayout;
 import java.awt.Dimension;
 import java.awt.FlowLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.util.ArrayList;
 
+import javax.swing.DefaultListModel;
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
@@ -14,10 +18,13 @@ import javax.swing.JPanel;
 import javax.swing.JScrollPane;
 
 import BusinessLayer.EditOrderDetails;
+import BusinessLayer.OrderSummary;
 import BusinessLayer.CustomerClasses.Customer;
 import BusinessLayer.OrderClasses.Order;
+import BusinessLayer.ProductClasses.Product;
 
-public class CartUI {
+public class CartUI extends JPanel{
+	int counter = 0;
 
 	public CartUI(Order userOrder,Customer currentCustomer) {
 		System.out.println("[debug] : ****** Starting CartUI Class  ******");
@@ -26,8 +33,47 @@ public class CartUI {
 // ************ 	Start JFrame stuff
 // ************************************************************************************************************************
 
+		JList list;
+		ArrayList<Product> orderProducts = userOrder.getProducts();
+
+		  DefaultListModel model;
+
+		
 		JFrame cartUiFrame = new JFrame("Your Details - please review your details");
-		cartUiFrame.setLayout(new FlowLayout());
+		cartUiFrame.setLayout(new BorderLayout());
+		model = new DefaultListModel();
+		list = new JList(model);
+		JScrollPane pane = new JScrollPane(list);
+	    JButton addButton = new JButton("Add Element");
+	    JButton removeButton = new JButton("Remove Element");
+	    for (int i = 0; i < orderProducts.size(); i++)
+	        model.addElement("Product " + orderProducts.get(i).getProductDetails());
+
+	      addButton.addActionListener(new ActionListener() {
+	        public void actionPerformed(ActionEvent e) {
+	          try {
+				ProductListUI p = new ProductListUI(currentCustomer);
+			} catch (IOException e1) {
+				// TODO Auto-generated catch block
+				e1.printStackTrace();
+			}
+	        }
+	      });
+	      removeButton.addActionListener(new ActionListener() {
+	        public void actionPerformed(ActionEvent e) {
+	          //if (model.getSize() > 0)
+	            //model.removeElementAt(0);
+	        	
+	            int selectedIndex =list.getSelectedIndex();
+	            if (selectedIndex != -1) {
+	            	model.removeElementAt(selectedIndex);
+	            	orderProducts.remove(selectedIndex);
+	            	userOrder.setProducts(orderProducts);
+	            }	          
+	        }
+	      });
+	      
+	      
 		cartUiFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 	    //String[] orderProductNames = userOrder.getOrderProductNames();
 	    //JList list = new JList(orderProductNames);
@@ -37,7 +83,7 @@ public class CartUI {
 	    //cartUiFrame.add(new JScrollPane(list));
 
 		JLabel uiNotReadyLabel = new JLabel();
-		uiNotReadyLabel.setText("This UI is under construction. You will be able to review your Ordered products here.");
+		//uiNotReadyLabel.setText("This UI is under construction. You will be able to review your Ordered products here.");
 		
 	    JButton purchaseButton = new JButton("Proceed to Details");
    		purchaseButton.addActionListener(new ActionListener() {
@@ -47,13 +93,24 @@ public class CartUI {
 
 		    	cartUiFrame.setVisible(false);
 		    	cartUiFrame.dispose();
-		        EditOrderDetailsUI createNewOrderSummaryUI = new EditOrderDetailsUI(userOrder, currentCustomer);
+		    	userOrder.setProducts(orderProducts);
+		        //EditOrderDetailsUI createNewOrderSummaryUI = new EditOrderDetailsUI(userOrder, currentCustomer);
+		    	try {
+					OrderSummary orderSum = new OrderSummary(userOrder,currentCustomer);
+				} catch (FileNotFoundException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
 		    }
 		});
    		
    		JPanel controls = new JPanel(new FlowLayout(FlowLayout.CENTER,5,2));
-   		controls.add(purchaseButton);
+   		
    		controls.add(uiNotReadyLabel);
+   		controls.add(pane, BorderLayout.NORTH);
+	      controls.add(addButton, BorderLayout.WEST);
+	      controls.add(removeButton, BorderLayout.EAST);
+	      controls.add(purchaseButton, BorderLayout.SOUTH);
 
 	    cartUiFrame.pack();
    		cartUiFrame.setContentPane(controls);
