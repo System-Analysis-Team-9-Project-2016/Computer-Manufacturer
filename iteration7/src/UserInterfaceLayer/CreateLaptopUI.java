bbfe9f13376fde8da351ae5b53f7eb688b54e758
danieloc <daniel.o.c.03@gmail.com>
2016-04-24 16:07:48 +0100

    Fix LaptopUI

--- iteration7/src/UserInterfaceLayer/CreateLaptopUI.java
+++ iteration7/src/UserInterfaceLayer/CreateLaptopUI.java
@@ -2,7 +2,6 @@ package UserInterfaceLayer;
 
 import java.awt.BorderLayout;
 import java.awt.Color;
-import java.awt.Container;
 import java.awt.Font;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
@@ -17,49 +16,46 @@ import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JList;
 import javax.swing.JPanel;
-import javax.swing.JScrollPane;
 
 import BusinessLayer.BusinessLayerDataControl;
 import BusinessLayer.ProductList;
 import BusinessLayer.CustomerClasses.Customer;
-import BusinessLayer.OrderClasses.Order;
 import BusinessLayer.ProductClasses.Product;
-import BusinessLayer.ProductClasses.Desktop;
+import BusinessLayer.ProductClasses.Laptop;
 import BusinessLayer.ProductClasses.Component;
-import DataLayer.DataControl;
 
 @SuppressWarnings("serial")
-public class CreateLaptopUI extends JPanel{
+public class CreatePCUI extends JPanel{
 	
-	private JList choices;
-	private JList chosen;
+	private JList listOfChoices;
+	private JList listOfChosenProducts;
 	private ArrayList<Product> productsInFile;
 	private DefaultListModel<String> model;
 	private DefaultListModel<String> modelChosen;
-	private Desktop currentDesk= new Desktop(1234, "Boaty McBoatFace", 1, 0.0, true, false, "Windows", true);
+	private Desktop currentDesk= new Laptop(1234, "Boaty McBoatFace", 1, 0.0, true, false, "Windows", String OS, true, 17, 4000, 1000);
 	private int q = 0;
 	JPanel topPanel = new JPanel();
 	JPanel bottomPanel = new JPanel();
 	JPanel m = new JPanel();
 	JPanel p = new JPanel();
-	JFrame CreateLaptopUiFrame = new JFrame("Create laptop model:");
+	JFrame CreatePCUiFrame = new JFrame("Create PC modal:");
 	JLabel currentLabel = new JLabel("Add a component to the list");
 	private boolean chooseArrayMade = false;
 	private boolean chosenArrayMade = false;
 	private boolean backButtonClicked = false;
 
-	public CreateLaptopUI(Customer currentCustomer) {
-		System.out.println("[debug] : ****** Starting CreateLaptopUI Class  ******");
+	public CreatePCUI(Customer currentCustomer) {
+		System.out.println("[debug] : ****** Starting CreatePCUI Class  ******");
 
 		// ************************************************************************************************************************
 		// ************ 	Start JFrame stuff
 		// ************************************************************************************************************************
 		
-		JFrame CreateLaptopUiFrame = new JFrame("Create laptop model:");
-		CreateLaptopUiFrame.setBackground(new Color(0,100,200));
-		CreateLaptopUiFrame.setLayout(new BorderLayout(200,200));
+		JFrame CreatePCUiFrame = new JFrame("Create PC modal:");
+		CreatePCUiFrame.setBackground(new Color(0,100,200));
+		CreatePCUiFrame.setLayout(new BorderLayout(200,200));
 		JLabel productsListJLabel = new JLabel("Products");
-		CreateLaptopUiFrame.add(productsListJLabel, BorderLayout.NORTH);
+		CreatePCUiFrame.add(productsListJLabel, BorderLayout.NORTH);
 		productsListJLabel.setFont(new Font("Dialog",Font.BOLD,20));
 		model = new DefaultListModel<String>();
 		modelChosen = new DefaultListModel<String>();
@@ -83,9 +79,8 @@ public class CreateLaptopUI extends JPanel{
 		buyBtn.addActionListener(new ActionListener() {
 			@Override
 			public void actionPerformed(ActionEvent evt) {
-				System.out.println("[info]  : ------ Buy button clicked (CreateLaptopUI Customer) ------");
+				System.out.println("[info]  : ------ Buy button clicked (CreatePCUI Customer) ------");
 				try {
-					CreateLaptopUiFrame.setVisible(false);
 					ProductList createNewProductList = new ProductList(currentDesk.getComponents(), currentCustomer);
 				} 
 				catch (IOException e) {
@@ -100,13 +95,9 @@ public class CreateLaptopUI extends JPanel{
 		addBtn.addActionListener(new ActionListener() {
 			@Override
 			public void actionPerformed(ActionEvent evt) {
-				System.out.println("[info]  : ------ Add button clicked (CreateLaptopUI Customer) ------");
+				System.out.println("[info]  : ------ Add button clicked (CreatePCUI Customer) ------");
 
-				int indexOfSelectedComponents = choices.getSelectedIndex();
-				System.out.println(indexOfSelectedComponents);
-				if (indexOfSelectedComponents == -1){
-					indexOfSelectedComponents = 0;
-				}
+				
 				System.out.println(q);
 				String part ="";
 				switch(q) {
@@ -118,18 +109,23 @@ public class CreateLaptopUI extends JPanel{
 				}
 				int y =0;
 				int u = 0;
-
+				
+				int indexOfSelectedComponents = listOfChoices.getSelectedIndex();
+				if (indexOfSelectedComponents == -1){
+					indexOfSelectedComponents = 0;
+				}
+				System.out.println(indexOfSelectedComponents);
+				
 				while (y < productsInFile.size() && u <=indexOfSelectedComponents) {
 
-					System.out.println("**************");
+					System.out.println("y"+y);
 					Component someProduct = productsInFile.get(y);
-					if(someProduct.getProductName().substring(0, Math.min(someProduct.getProductName().length(), 3)).equals(part)) {
+					if(someProduct.getProductName().equals(part)) {
 						u++;
 					}
 					y++;
-
 				}
-				currentDesk.addProduct(productsInFile.get(y+u-2));
+				currentDesk.addProduct(productsInFile.get(y-1));
 				q++;
 				populateArrayOfProducts();
 			}
@@ -139,12 +135,12 @@ public class CreateLaptopUI extends JPanel{
 		backBtn.addActionListener(new ActionListener() {
 			@Override
 			public void actionPerformed(ActionEvent evt) {
-				System.out.println("[info]  : ------ Back button clicked (CreateLaptopUI Customer) ------");
+				System.out.println("[info]  : ------ Back button clicked (CreatePCUI Customer) ------");
 				if(chosenArrayMade){
 					currentDesk.getComponents().remove(currentDesk.getComponents().size() - 1);
-					if ( q >= 4) {
+					if ( q > 8) {
 						m.add(addBtn);
-						m.add(choices);
+						m.add(listOfChoices);
 						currentLabel.setVisible(true);
 					}
 					q--;
@@ -163,12 +159,14 @@ public class CreateLaptopUI extends JPanel{
 
 	}
 	public void populateArrayOfProducts() {
-		if (choices != null ) {
-			choices.removeAll();
+		if (listOfChoices != null ) {
+			m.remove(listOfChoices);
+			listOfChoices.removeAll();
+			chooseArrayMade = false;
 			
 		}
-		if(chosen != null) {
-			chosen.removeAll();
+		if(listOfChosenProducts != null) {
+			listOfChosenProducts.removeAll();
 		}
 		if (modelChosen != null) {
 			modelChosen.removeAllElements();
@@ -185,14 +183,18 @@ public class CreateLaptopUI extends JPanel{
 			case 1 : part = "CPU"; break;
 			case 2 : part = "GPU"; break;
 			case 3 : part = "RAM"; break;
-			case 4 : part = "MemoryDrives"; break;
+			case 4 : part = "MememoryDrives"; break;
+			case 5 : part = "Monitor"; break;
+			case 6 : part = "Keyboard"; break;
+			case 7 : part = "Mouse"; break;
+			case 8 : part = "Speaker"; break;
 			}
 			for(int i = 0; i < productsInFile.size(); i++){
 				Product someProduct = productsInFile.get(i);
 				if(someProduct.getProductName().equals(part)) {
 					model.addElement(someProduct.getProductUIDetails());
 				}
-				choices = new JList(model);
+				listOfChoices = new JList(model);
 			}
 			if (model.isEmpty()) {
 				if (backButtonClicked == true)
@@ -202,7 +204,7 @@ public class CreateLaptopUI extends JPanel{
 					
 				}
 				else {
-					if(q > 5) {
+					if(q > 8) {
 						endOfFileReached = true;
 						m.removeAll();
 						currentLabel.setVisible(false);
@@ -215,9 +217,9 @@ public class CreateLaptopUI extends JPanel{
 			}
 
 		}
-		if (choices != null && !chooseArrayMade)
+		if (listOfChoices != null && !chooseArrayMade)
 		{
-			m.add(choices);
+			m.add(listOfChoices);
 			chooseArrayMade = true;
 		}
 		String labelPart ="";
@@ -226,9 +228,9 @@ public class CreateLaptopUI extends JPanel{
 		case "CPU" : labelPart = "CPU"; break;
 		case "GPU" : labelPart = "GPU"; break;
 		case "RAM" : labelPart = "RAM"; break;
-		case "MemoryDrives" : labelPart = "MemoryDrives"; break;
+		case "MemoryDrives" : labelPart = "Memory Drive"; break;
 		}
-		currentLabel.setText("Please pick a " + labelPart + " for your Laptop" );
+		currentLabel.setText("Please pick a " + labelPart + " for your PC" );
 		
 		
 		int i = 0;
@@ -239,19 +241,19 @@ public class CreateLaptopUI extends JPanel{
 			Product someComp = it.next();
 			System.out.println(someComp.getProductName());
 			modelChosen.addElement(someComp.getProductUIDetails());
-			chosen = new JList(modelChosen);
+			listOfChosenProducts = new JList(modelChosen);
 			
 		}
 		
-		if (chosen != null && !chosenArrayMade){
-			p.add(chosen);
+		if (listOfChosenProducts != null && !chosenArrayMade){
+			p.add(listOfChosenProducts);
 			chosenArrayMade = true;
 		}
 		topPanel.add(currentLabel);
-		CreateLaptopUiFrame.add(bottomPanel, BorderLayout.SOUTH);
-		CreateLaptopUiFrame.add(topPanel, BorderLayout.NORTH);
-		CreateLaptopUiFrame.add(m, BorderLayout.WEST);
-		CreateLaptopUiFrame.add(p, BorderLayout.EAST);
+		CreateLaptopUIFrame.add(bottomPanel, BorderLayout.SOUTH);
+		CreateLaptopUIFrame.add(topPanel, BorderLayout.NORTH);
+		CreateLaptopUIFrame.add(m, BorderLayout.WEST);
+		CreateLaptopUIFrame.add(p, BorderLayout.EAST);
 		CreateLaptopUiFrame.setSize(800, 500);
 		CreateLaptopUiFrame.setVisible(true);
 	}
