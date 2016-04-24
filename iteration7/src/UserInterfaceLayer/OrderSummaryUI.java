7cfa7009e3c2b44ad5c5a4f2d08d177ba45e3cc3
conorjduggan-ul <13127004@studentmail.ul.ie>
2016-04-24 14:33:32 +0100

    make receipt scrollable

--- iteration7/src/UserInterfaceLayer/OrderSummaryUI.java
+++ iteration7/src/UserInterfaceLayer/OrderSummaryUI.java
@@ -26,28 +26,18 @@ System.out.println("[debug] : ****** Starting OrderSummaryUI Class ******");
 		    	System.exit(0);
 		    }
 		});
-/*
-   		JButton goToProductListButton = new JButton("Go to product list");
-   		goToProductListButton.addActionListener(new ActionListener() {
-		    @Override
-		    public void actionPerformed(ActionEvent evt) {
-		    	System.out.println("[info]  : ------ Go to product list button clicked (OrderSummaryUI) ------");
-		    	cartUiFrame.setVisible(false);
-		    	cartUiFrame.dispose();
-		    	//ProductList createNewProductList = new ProductList(currentCustomer);
-		    }
-		});
-*/
+   		
 		JLabel orderOuputLabel = new JLabel("<html>" + postOrderDetails + "</html>");
+		
+		JScrollPane scroller = new JScrollPane(orderOuputLabel, JScrollPane.VERTICAL_SCROLLBAR_AS_NEEDED, JScrollPane.HORIZONTAL_SCROLLBAR_AS_NEEDED);
    		
    		JPanel controls = new JPanel(new FlowLayout(FlowLayout.CENTER,5,2));
    		controls.add(exitButton);
-      	//controls.add(goToProductListButton);
 
    		JPanel gui = new JPanel(new BorderLayout(10,10));
         gui.setBorder(new TitledBorder("Thank you for shopping with us"));
         gui.add(controls, BorderLayout.SOUTH);
-        gui.add(orderOuputLabel, BorderLayout.CENTER);
+        gui.add(scroller, BorderLayout.CENTER);
 
         cartUiFrame.setContentPane(gui);
         cartUiFrame.pack();
